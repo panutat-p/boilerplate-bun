035ad891c50af0af039a76236b9ee9a1bd10803c
panutat.p <pp.panutat@gmail.com>
2024-11-24 00:31:09 +0700

    feat: luxon

--- src/luxon.ts
+++ src/luxon.ts
@@ -3,3 +3,11 @@ import { DateTime } from 'luxon'
 export function now(): string {
   return DateTime.local().toISO()
 }
+
+export function toRFC3339(d: DateTime): string {
+  return d.toISODate() || ''
+}
+
+export function parse(s: string): DateTime {
+  return DateTime.fromISO(s)
+}
